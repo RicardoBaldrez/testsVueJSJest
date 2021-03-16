e801ef8bd4bf6cba5652d6737862a90986369ae3
Ricardo Barbosa Baldrez <ricardo.baldrez@gmail.com>
2021-03-16 17:22:26 -0300

    Teste que garante que o valor do lance seja maior que o valor de lance minimo

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -19,31 +19,63 @@ test('Garantindo que o componente Lance foi realmente montado', () => {
     expect(wrapper).toBeTruthy()
 })
 
-test('Garantindo que o usuário não de um lance menor que zero', () => {
-    const wrapper = mount(Lance)
-    const inputLance = wrapper.find('input') // find -> Procura esse elemento html dentro do wrapper(componente Lance)
-    inputLance.setValue(-100) // setValeu -> Seta um valor dentro do input
-    const lancesEmitidos = wrapper.emitted('novo-lance') // Esperando que o evento 'novo-lance' seja emitido
-    wrapper.trigger('submit')
-    expect(lancesEmitidos).toBeUndefined()
+describe('Um lance sem valor minimo', () => {
+    test('Garantindo que o usuário não de um lance menor que zero', () => {
+        const wrapper = mount(Lance)
+        const inputLance = wrapper.find('input') // find -> Procura esse elemento html dentro do wrapper(componente Lance)
+        inputLance.setValue(-100) // setValeu -> Seta um valor dentro do input
+        const lancesEmitidos = wrapper.emitted('novo-lance') // Esperando que o evento 'novo-lance' seja emitido
+        wrapper.trigger('submit')
+        expect(lancesEmitidos).toBeUndefined()
+    })
+    
+    test('Garantindo que seja emitido um novo lance quando o valor for maior que zero', () => {
+        const wrapper = mount(Lance)
+        const inputLance = wrapper.find('input')
+        inputLance.setValue(100)
+        wrapper.trigger('submit')
+        const lancesEmitidos = wrapper.emitted('novo-lance')
+        expect(lancesEmitidos).toHaveLength(1)
+    })
+    
+    test('Garantindo a emissão de um valor esperado de um lance válido', () => {
+        const wrapper = mount(Lance)
+        const inputLance =wrapper.find('input')
+        inputLance.setValue(200)
+        wrapper.trigger('submit')
+        const lancesEmitidos = wrapper.emitted('novo-lance')
+        const lance = parseInt(lancesEmitidos[0][0])
+        expect(lance).toBe(200)
+    })
 })
 
-test('Garantindo que seja emitido um novo lance quando o valor for maior que zero', () => {
-    const wrapper = mount(Lance)
-    const inputLance = wrapper.find('input')
-    inputLance.setValue(100)
-    wrapper.trigger('submit')
-    const lancesEmitidos = wrapper.emitted('novo-lance')
-    expect(lancesEmitidos).toHaveLength(1)
-})
+describe('Um lance com valor minimo', () => {
+    test('Garantindo que todos os lances tenham um valor maior que o lance minimo informado', () => {
+        const wrapper = mount(Lance, {
+            propsData: {
+                lanceMinimo: 300
+            }
+        })
 
-test('Garantindo a emissão de um valor esperado de um lance válido', () => {
-    const wrapper = mount(Lance)
-    const inputLance =wrapper.find('input')
-    inputLance.setValue(200)
-    wrapper.trigger('submit')
-    const lancesEmitidos = wrapper.emitted('novo-lance')
-    console.log(lancesEmitidos);
-    const lance = parseInt(lancesEmitidos[0][0])
-    expect(lance).toBe(200)
+        const input = wrapper.find('input')
+        input.setValue(400)
+        wrapper.trigger('submit')
+        const lancesEmitidos = wrapper.emitted('novo-lance')
+        expect(lancesEmitidos).toHaveLength(1)
+    })
+
+    test('Garantindo a emissão de um valor esperado de um lance válido', () => {
+        const wrapper = mount(Lance, {
+            propsData: {
+                lanceMinimo: 300
+            }
+        })
+
+        const input = wrapper.find('input')
+        input.setValue(301)
+        wrapper.trigger('submit')
+        const lancesEmitidos = wrapper.emitted('novo-lance')
+        const lance = parseInt(lancesEmitidos[0][0])
+        expect(lance).toBe(301)
+    })
 })
